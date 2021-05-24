ad5ff733aacf4136413fcea329824f715eefa0d2
hoanganhnguyen123 <64483607+hoanganhnguyen123@users.noreply.github.com>
2021-05-24 20:35:13 +0300

    h

--- Bouncegame/game.py
+++ Bouncegame/game.py
@@ -1,13 +1,27 @@
 from random import randrange as rnd
-
+import socket
 import pygame as pg
 import matplotlib.pyplot as plt
+import paho.mqtt.client as mqtt
+import time
 
+HOST = "localhost"
+PORT = 1883
+MQTT_USER = "user"
+MQTT_PASS = "password"
+
+Client = mqtt.Client('client1')
+Client.username_pw_set(MQTT_USER,MQTT_PASS)
+
+Client.connect(HOST,PORT)
+Client.loop_start()
+UDP_IP = "127.0.0.1"
+UDP_PORT = 8094
 WIDTH, HEIGHT = 800, 600
-
+#chứa tọa độ
 ball_coordinates = []
 paddle_coordinates = [] 
-
+#định nghĩa hàm
 def check_if_coordinate_exist(coordinate, list_of_coordinates):
     if not list_of_coordinates:
         return False
@@ -15,6 +29,7 @@ def check_if_coordinate_exist(coordinate, list_of_coordinates):
     if last_cor["x"] == coordinate["x"] and last_cor["y"] == coordinate["y"]:
         return True
     return False
+#định nghĩa hàm
 
 def draw(coordinates):
     plt.gca().invert_yaxis()
@@ -45,7 +60,7 @@ clock = pg.time.Clock()
 # background color
 bg = pg.Surface((WIDTH, HEIGHT))
 bg.fill((21, 21, 21))
-
+# phát hiện qua chạm
 def detect_collision(dx, dy, ball, rect):
     if dx > 0:
         delta_x = ball.right - rect.left
@@ -91,24 +106,33 @@ while True:
         fps += 2
     if ball.bottom > HEIGHT:
         print('GAME OVER!')
+        # khi kết thúc vẽ biểu đồ quỹ đạo
         draw(ball_coordinates)
         draw(paddle_coordinates)
         exit()
     elif not len(block_list):
         print('WIN!!!')
         exit()
+        #sự kiện bắt bàn phím
     key = pg.key.get_pressed()
     if key[pg.K_LEFT] and paddle.left > 0:
         paddle.left -= paddle_speed
     if key[pg.K_RIGHT] and paddle.right < WIDTH:
         paddle.right += paddle_speed
-
+# kiểm tra vị trí nếu có chưa, nếu chưa thì thêm vào
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     paddle_cor = {"x": paddle.centerx, "y": paddle.centery}
     ball_cor = {"x": ball.centerx, "y": ball.centery}
     if not check_if_coordinate_exist(paddle_cor, paddle_coordinates):
         paddle_coordinates.append(paddle_cor)
+        MESSAGE_PADDLE = b"coord,obj=paddle x_coord=%d,y_coord=%d " % (paddle.centerx, paddle.centery)
+        sock.sendto(MESSAGE_PADDLE, (UDP_IP, UDP_PORT))
+        Client.publish("TOPIC_PANDDLE", f"x={paddle.centerx},y={paddle.centery}")
     if not check_if_coordinate_exist(ball_cor, ball_coordinates):
         ball_coordinates.append(ball_cor)
-
+        MESSAGE_BALL = b"coord,obj=ball x_coord=%d,y_coord=%d " % (ball.centerx, ball.centery)
+        sock.sendto(MESSAGE_BALL, (UDP_IP, UDP_PORT))
+        Client.publish("TOPIC_BALL",f"x={ball.centerx},y={ball.centery}")
+        
     pg.display.flip()
     clock.tick(fps)
